74733246b3b411710c9434adbe18d4c07b36e27c
Damilola Jerugba <damiisdandy@icloud.com>
2023-01-20 10:34:25 +0100

    Updated  Message Serializer

--- message/serializers.py
+++ message/serializers.py
@@ -3,6 +3,8 @@ from message.models import Message
 
 
 class MessageSerializer(serializers.ModelSerializer):
+    source = serializers.CharField(write_only=True)
+
     class Meta:
         model = Message
-        fields = ['id', 'text', 'created_at']
+        fields = ['id', 'text', 'source', 'created_at']
